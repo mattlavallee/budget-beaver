fab62c4d1a1f8159f393664a29f2c7c247981814
Matt Lavallee <matthew.t.lavallee@gmail.com>
2016-10-16 17:28:13 -0400

    feat(transaction): adding ability to save a new transaction

--- BudgetBeaver/app/src/main/java/io/github/mattlavallee/budgetbeaver/fragments/EditTransactionFragment.java
+++ BudgetBeaver/app/src/main/java/io/github/mattlavallee/budgetbeaver/fragments/EditTransactionFragment.java
@@ -1,23 +1,31 @@
 package io.github.mattlavallee.budgetbeaver.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import io.github.mattlavallee.budgetbeaver.BudgetBeaverFabSetup;
 import io.github.mattlavallee.budgetbeaver.R;
 import io.github.mattlavallee.budgetbeaver.data.DatabaseDispatcher;
 import io.github.mattlavallee.budgetbeaver.models.Account;
+import io.github.mattlavallee.budgetbeaver.models.Transaction;
 
 public class EditTransactionFragment extends Fragment {
     private DatabaseDispatcher dbDispatcher;
@@ -42,7 +50,7 @@ public class EditTransactionFragment extends Fragment {
         RelativeLayout parent = (RelativeLayout)getActivity().findViewById(R.id.budget_beaver_fragment_wrapper);
         BudgetBeaverFabSetup.removeExistingFab(parent);
 
-        initializeButtons( fragmentView );
+        initializeButtons( fragmentView, transactionId, accountId );
         populateAccountSpinner( fragmentView, accountId );
 
         return fragmentView;
@@ -52,7 +60,7 @@ public class EditTransactionFragment extends Fragment {
         getActivity().getSupportFragmentManager().popBackStackImmediate();
     }
 
-    private void initializeButtons(View view ){
+    private void initializeButtons(final View view, final int transactionId, final int accountId ){
         Button cancel = (Button)view.findViewById(R.id.edit_transaction_cancel_btn);
         Button save = (Button)view.findViewById(R.id.edit_transaction_save_btn);
 
@@ -65,11 +73,50 @@ public class EditTransactionFragment extends Fragment {
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //TODO: actually save something
-                closeEditTransactionFragment();
+            public void onClick(View v) {
+                saveTransaction( view, transactionId, accountId );
             }
         });
+    }
+
+    private void saveTransaction( View view, int transactionId, int accountId ){
+        EditText location = (EditText)view.findViewById(R.id.edit_transaction_location);
+        EditText amount = (EditText)view.findViewById(R.id.edit_transaction_amount);
+        EditText description = (EditText)view.findViewById(R.id.edit_transaction_description);
+        DatePicker date = (DatePicker)view.findViewById(R.id.edit_transaction_date);
+        if(accountId == -1){
+            //TODO: get the account to save the transaction to
+        }
+        String transLocation = location.getText().toString();
+        String transDescription = description.getText().toString();
+        double transAmount = 0.0;
+        if(!amount.getText().toString().isEmpty()){
+            transAmount = Double.parseDouble(amount.getText().toString());
+        }
+        Date transDate = new GregorianCalendar(date.getYear(), date.getMonth(), date.getDayOfMonth()).getTime();
+
+        if(transLocation.length() < 3){
+            displaySnack("Location must be at least 3 letters", view);
+            return;
+        } else if( transAmount == 0.0){
+            displaySnack("Transaction must have a non-zero value", view);
+            return;
+        }
+
+        Transaction transToSave = new Transaction(transactionId, accountId, transLocation,
+                transDescription, transAmount, transDate, true);
+        long result;
+        if(transactionId == -1){
+            result = dbDispatcher.Transactions.insertTransaction(transToSave);
+        } else{
+            result = dbDispatcher.Transactions.updateTransaction(transToSave);
+        }
+
+        if(result == -1){
+            displaySnack("There was an error saving the transaction", view);
+            return;
+        }
+        closeEditTransactionFragment();
     }
 
     private void populateAccountSpinner(View view, int accountId){
@@ -100,5 +147,12 @@ public class EditTransactionFragment extends Fragment {
         if(accountId != -1){
             accountDropdown.setEnabled(false);
         }
+    }
+
+    private void displaySnack(String message, View view) {
+        Snackbar snack = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        TextView snackText = (TextView) snack.getView().findViewById(android.support.design.R.id.snackbar_text);
+        snackText.setTextColor(Color.CYAN);
+        snack.show();
     }
 }
