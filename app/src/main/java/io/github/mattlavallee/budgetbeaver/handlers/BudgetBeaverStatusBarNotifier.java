7cab2ac0210a755c59240237018d0252fdeb18a0
Matt Lavallee <matthew.t.lavallee@gmail.com>
2017-10-02 11:01:12 -0400

    feat(service): updating background service to generate notifications

--- app/src/main/java/io/github/mattlavallee/budgetbeaver/handlers/BudgetBeaverStatusBarNotifier.java
+++ app/src/main/java/io/github/mattlavallee/budgetbeaver/handlers/BudgetBeaverStatusBarNotifier.java
@@ -1,16 +1,14 @@
 package io.github.mattlavallee.budgetbeaver.handlers;
 
 import android.app.Activity;
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v4.app.NotificationCompat;
 
 import java.util.ArrayList;
@@ -49,6 +47,40 @@ public class BudgetBeaverStatusBarNotifier {
 
             NotificationManager mNotificationManager = (NotificationManager)
                     activity.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            // notificationID allows you to update the notification later on.
+            String uuid = "budget beaver_" + newNotification.getId();
+            mNotificationManager.notify(uuid.hashCode(), notifyBuilder.build());
+        }
+    }
+
+    public static void generateFromService(IntentService service, ArrayList<Reminder> notifications) {
+        DatabaseDispatcher dbDispatcher = new DatabaseDispatcher(service.getApplicationContext());
+
+        for(Reminder newNotification : notifications){
+            NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(service);
+            notifyBuilder.setSmallIcon(R.mipmap.ic_stat_check);
+            Bitmap bmp = BitmapFactory.decodeResource(service.getResources(), R.mipmap.ic_budget_beaver);
+            notifyBuilder.setLargeIcon(bmp);
+            notifyBuilder.setColor(Color.WHITE);
+            notifyBuilder.setContentTitle("Budget Beaver");
+            Account notificationAccount = dbDispatcher.Accounts.getAccountById( newNotification.getAccountId() );
+            notifyBuilder.setContentText( notificationAccount.getName() + ": " + newNotification.getMessage() );
+
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            bigTextStyle.setBigContentTitle("Budget Beaver");
+            bigTextStyle.bigText(notificationAccount.getName() + ": " + newNotification.getMessage());
+            notifyBuilder.setStyle(bigTextStyle);
+
+            //we want to launch BudgetBeaver on the notifications fragment when a notification is tapped
+            Intent resultIntent = new Intent(service, BudgetBeaverActivity.class);
+            resultIntent.putExtra("startOnNotifications", true);
+            PendingIntent resultPendingIntent = PendingIntent.getActivity(service, 0, resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            notifyBuilder.setContentIntent(resultPendingIntent);
+
+            NotificationManager mNotificationManager = (NotificationManager)
+                    service.getSystemService(Context.NOTIFICATION_SERVICE);
 
             // notificationID allows you to update the notification later on.
             String uuid = "budget beaver_" + newNotification.getId();
--- app/src/main/java/io/github/mattlavallee/budgetbeaver/service/ReminderService.java
+++ app/src/main/java/io/github/mattlavallee/budgetbeaver/service/ReminderService.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.ArrayList;
+
+import io.github.mattlavallee.budgetbeaver.data.DatabaseDispatcher;
+import io.github.mattlavallee.budgetbeaver.handlers.BudgetBeaverStatusBarNotifier;
+import io.github.mattlavallee.budgetbeaver.models.Reminder;
+
 public class ReminderService extends IntentService {
     public ReminderService() {
         super("ReminderService");
@@ -27,9 +33,29 @@ public class ReminderService extends IntentService {
         return Service.START_NOT_STICKY;
     }
 
+    private ArrayList<Reminder> invalidateOldAndActivateNewNotifications(DatabaseDispatcher dbDispatcher){
+        ArrayList<Reminder> allReminders = dbDispatcher.Reminders.getReminders();
+
+        ArrayList<Reminder> expiredReminders = Reminder.invalidateExpiredNotifications( allReminders );
+        ArrayList<Reminder> newActiveReminders = Reminder.activateNewNotifications( allReminders );
+
+        for(Reminder expReminder : expiredReminders ){
+            dbDispatcher.Reminders.updateReminder(expReminder);
+        }
+        for(Reminder actReminder : newActiveReminders ){
+            dbDispatcher.Reminders.updateReminder(actReminder);
+        }
+
+        return newActiveReminders;
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i("TEST", "do all the things!");
+        DatabaseDispatcher dbDispatcher = new DatabaseDispatcher(getApplicationContext());
+        ArrayList<Reminder> newNotifications = invalidateOldAndActivateNewNotifications(dbDispatcher);
+        BudgetBeaverStatusBarNotifier.generateFromService(this, newNotifications);
+
         this.stopSelf();
     }
 }
