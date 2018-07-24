c78b7be1bf43ca40b697bb190595271c314fc37f
Matt Lavallee <matthew.t.lavallee@gmail.com>
2018-07-23 20:56:07 -0400

    chore: api upgrade and emergency bug fix

--- app/src/main/java/io/github/mattlavallee/budgetbeaver/service/ReminderEventReceiver.java
+++ app/src/main/java/io/github/mattlavallee/budgetbeaver/service/ReminderEventReceiver.java
@@ -3,11 +3,16 @@ package io.github.mattlavallee.budgetbeaver.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class ReminderEventReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
         Intent eventService = new Intent(context, ReminderService.class);
-        context.startService(eventService);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(eventService);
+        } else {
+            context.startService(eventService);
+        }
     }
 }
